5b05527f1076f5580f2e0ad7198e99780aa9d4b5
Rain <rainine4@gmail.com>
2015-12-17 15:12:34 +0800

    finish DB.py

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -1,6 +1,55 @@
 # -*- coding: utf-8 -*-
 
-import threading, functool
+import threading, functools
+
+class Dict(dict):
+	'''
+    Simple dict but support access as x.y style.
+    >>> d1 = Dict()
+    >>> d1['x'] = 100
+    >>> d1.x
+    100
+    >>> d1.y = 200
+    >>> d1['y']
+    200
+    >>> d2 = Dict(a=1, b=2, c='3')
+    >>> d2.c
+    '3'
+    >>> d2['empty']
+    Traceback (most recent call last):
+        ...
+    KeyError: 'empty'
+    >>> d2.empty
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'Dict' object has no attribute 'empty'
+    >>> d3 = Dict(('a', 'b', 'c'), (1, 2, 3))
+    >>> d3.a
+    1
+    >>> d3.b
+    2
+    >>> d3.c
+    3
+    '''
+	def __init__(self, key = (), value = (), **kw):
+		super(Dict, self).__init__(**kw)
+		for k, v in zip(key, value):
+			self[k] = v
+
+	def __getattr__(self, key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+	def __setattr__(self, key, value):
+		self[key] = value
+
+class DBError(Exception):
+	pass
+
+class MultiColumnError(DBError):
+	pass
 
 engine = None
 
@@ -79,10 +128,113 @@ def connection():
 	return _ConnectionCtx()
 
 def with_connection(func):
-	@functools.warps(func)
-	def _warpper(*args, *kw):
+	@functools.wraps(func)
+	def _wrapper(*args, **kw):
 		with _ConnectionCtx():
-			return func(*args, *kw)
-	return _warpper
+			return func(*args, **kw)
+	return _wrapper
 
+class _TransactionCtx(object):
+	def __enter__(self):
+		global _db_ctx
+		self.should_close_conn = False
+		if not _db_ctx.is_init():
+			_db_ctx.init()
+			self.should_close_conn = True
+		_db_ctx.transactions += 1
+		return self
 
+	def __exit__(self, exctype, excvalue, traceback):
+		global _db_ctx
+		_db_ctx.transactions -= 1
+		try:
+			if _db_ctx.transaction == 0:
+				if exctype is None:
+					self.commit()
+				else:
+					self.rollback()
+		finally:
+			if self.should_close_conn:
+				_db_ctx.cleanup()
+
+	def commit(self):
+		global _db_ctx
+		try:
+			_db_ctx.connection.commit()
+		except:
+			_db_ctx.connection.rollback()
+
+	def rollback(self):
+		global _db_ctx
+		_db_ctx.connection.rollback()
+
+def transaction():
+	return _TransactionCtx()
+
+def with_transatcion(func):
+	@functools.wrap(func)
+	def _wrapper(*args, **kw):
+		with _TransactionCtx():
+			return func(*args, **kw)
+	return _wrapper
+
+@with_connection
+def _select(sql, first, *args):
+	global _db_ctx
+	sql = sql.replace('?', '%s')
+	cursor = None
+	try:
+		cursor = _db_ctx.cursor()
+		cursor.execute(sql, args)
+		if cursor.description:
+			names = [x[0] for x in cursor.description]
+		if first:
+			values = cursor.fetchone()
+			if not values:
+				return None
+			return Dict(names, values)
+		values = cursor.fetchall()
+		return Dict(names, values)
+	finally:
+		if cursor:
+			cursor.close()
+		
+def select_one(sql, *args):
+	return _select(sql, True, *args)
+
+def select_int(sql, *args):
+	d = _select(sql, False, *args)
+	if len(d) == 1:
+		return d.values()[0]
+	else:
+		raise MultiColumnError('Expect only one row')
+
+def select(sql, *args):
+	return _select(sql, False, *args)
+
+@with_connection
+def _update(sql, *args):
+	global _db_ctx
+	cursor = None
+	sql = sql.replace('?', '%s')
+	try:
+		cursor = _db_ctx.connection.cursor()
+		cursor.execute(sql, args)
+		if _db_ctx.transactions == 0:
+			_db_ctx.connection.commit()
+		return cursor.rowcount()
+	finally:
+		if cursor:
+			cursor.close()
+
+def insert(table, **kw):
+	cols, args = zip(*kw.iteritems())
+	sql = 'insert into `%s` (%s) values (%s)' % (table, ','.join(['`%s`' % col for col in cols]), ','.join(['?' for i in range(len(cols))]))
+	return _update(sql, *args)
+
+def update(sql, *args):
+	return _update(sql, *args)
+
+if __name__ == '__main__':
+	import doctest
+	doctest.testmod()
